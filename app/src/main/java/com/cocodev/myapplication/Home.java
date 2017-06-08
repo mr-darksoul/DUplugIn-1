0b17154deb741f9e144a64093ca5b64b70964ade
Sudarshan Yadav <sudarshanyadav1997@gmail.com>
2017-06-08 11:51:37 +0530

    Add Home Page Preferences to Home Screen

--- app/src/main/java/com/cocodev/myapplication/Home.java
+++ app/src/main/java/com/cocodev/myapplication/Home.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
@@ -19,6 +20,12 @@ import android.widget.GridView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TabHost;
 import android.widget.TextView;
+
+import com.cocodev.myapplication.adapter.MyFragmentPageAdapter;
+import com.cocodev.myapplication.notices.Notices;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -52,13 +59,38 @@ public class Home extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_notice_board, container, false);
 
-       // initViewPager(view);
-        getActivity().setTitle("Notice Board");
+        initViewPager(view);
+        getActivity().setTitle("App Name");
 
 
         return view;
     }
+    private void initViewPager(View view) {
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager_home);
+        List<Notices> listFragmetns = new ArrayList<Notices>();
 
+        Notices classNotices = new Notices();
+        classNotices.setType(Notices.TYPE_CLASS);
+        listFragmetns.add(classNotices);
+
+        Notices collegeNotices = new Notices();
+        collegeNotices.setType(Notices.TYPE_COLLEGE);
+        listFragmetns.add(collegeNotices);
+
+
+        Notices allNotices = new Notices();
+        allNotices.setType(Notices.TYPE_ALL);
+        listFragmetns.add(allNotices);
+
+        MyFragmentPageAdapter fragmentPageAdapter = new MyFragmentPageAdapter(getFragmentManager(),listFragmetns);
+
+        viewPager.setAdapter(fragmentPageAdapter);
+        viewPager.setOffscreenPageLimit(3);
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout_home);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
 
 
     @Override
--- app/src/main/java/com/cocodev/myapplication/SettingsActivity.java
+++ app/src/main/java/com/cocodev/myapplication/SettingsActivity.java
@@ -175,7 +175,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         return PreferenceFragment.class.getName().equals(fragmentName)
                 || GeneralPreferenceFragment.class.getName().equals(fragmentName)
                 || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
-                || NotificationPreferenceFragment.class.getName().equals(fragmentName);
+                || NotificationPreferenceFragment.class.getName().equals(fragmentName)
+                || HomeFeedPreferenceFragment.class.getName().equals(fragmentName);
     }
 
     /**
@@ -189,7 +190,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
             setHasOptionsMenu(true);
-
             // Bind the summaries of EditText/List/Dialog/Ringtone preferences
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
@@ -257,6 +257,34 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // guidelines.
             bindPreferenceSummaryToValue(findPreference("sync_frequency"));
         }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class HomeFeedPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_home_feed);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference("home_feed_preferences"));
+
+        }
+
 
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
